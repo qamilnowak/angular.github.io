c891bec4cd7cb927364fe309951a313af197330d
qamilnowak <qamilnowak@gmail.com>
2017-11-03 22:01:30 +0100

    redirect to /cars

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import {NgModule} from '@angular/core';
 import {Route, RouterModule} from '@angular/router';
 import {CarsListComponent} from './cars/cars-list/cars-list.component';
 
-const APP_ROUTES: Route[] = [{path: 'cars', component: <any>CarsListComponent}];
+const APP_ROUTES: Route[] = [
+  {path: '', pathMatch: 'full', redirectTo: 'cars'},
+  {path: 'cars', component: <any>CarsListComponent}
+  ];
 @NgModule({
   imports: [RouterModule.forRoot(APP_ROUTES)
     ],
