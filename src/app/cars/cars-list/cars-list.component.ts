2d25075308b83376b6f21f0c4884ec2760c9c3ae
qamilnowak <qamilnowak@gmail.com>
2017-11-04 15:09:36 +0100

    router.navigate method added

--- src/app/cars/cars-list/cars-list.component.ts
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -3,6 +3,7 @@ import {AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation} from '@a
 import {Car} from '../models/car';
 import {TotalcostComponent} from '../totalcost/totalcost.component';
 import {CarsService} from '../cars.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-cars-list',
@@ -16,7 +17,8 @@ export class CarsListComponent implements OnInit, AfterViewInit {
   grossCost: number;
   cars: Car[];
 
-  constructor(private carsService: CarsService) {
+  constructor(private carsService: CarsService,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -33,7 +35,9 @@ export class CarsListComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     this.totalCostRef.showGross();
   }
-
+  goToCarDetails(car: Car) {
+    this.router.navigate(['/cars', car.id]);
+  }
   showGross(): void {
     this.totalCostRef.showGross();
   }
