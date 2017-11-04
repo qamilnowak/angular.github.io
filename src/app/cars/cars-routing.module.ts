6a27faca0ba4decf550be0ff58c2ad562e12eed9
qamilnowak <qamilnowak@gmail.com>
2017-11-04 16:13:07 +0100

    form template added

--- src/app/cars/cars-routing.module.ts
+++ src/app/cars/cars-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {Route, RouterModule} from '@angular/router';
 import {CarDetailsComponent} from './car-details/car-details.component';
-import {CarResolve} from "./car-resolve.service";
+import {CarResolve} from './car-resolve.service';
 
 const CARS_ROUTES: Route[] = [
   {
