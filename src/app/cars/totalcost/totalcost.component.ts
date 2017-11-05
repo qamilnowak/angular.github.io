e4595692c2e60582a6515698b86c168084e77a06
qamilnowak <qamilnowak@gmail.com>
2017-11-05 19:07:27 +0100

    Detection mechanism added

--- src/app/cars/totalcost/totalcost.component.ts
+++ src/app/cars/totalcost/totalcost.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-totalcost',
   templateUrl: './totalcost.component.html',
-  styleUrls: ['./totalcost.component.less']
+  styleUrls: ['./totalcost.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TotalcostComponent {
   @Input() totalCost: number;
   @Output() shownGross: EventEmitter<number> = new EventEmitter<number>();
   VAT: number = 1.23;
   showGross(): void {
+    setTimeout(() => {console.log('hey'); }, 1000);
     this.shownGross.emit(this.totalCost * this.VAT);
   }
 }
