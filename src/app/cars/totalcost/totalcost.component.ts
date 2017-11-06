ed633343ffe1e42da029a5b52caff6840e0b2062
qamilnowak <qamilnowak@gmail.com>
2017-11-06 23:33:12 +0100

    hook onChanges added

--- src/app/cars/totalcost/totalcost.component.ts
+++ src/app/cars/totalcost/totalcost.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, EventEmitter, Output, ChangeDetectionStrategy } from '@angular/core';
+import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-totalcost',
@@ -6,12 +6,22 @@ import { Component, Input, EventEmitter, Output, ChangeDetectionStrategy } from 
   styleUrls: ['./totalcost.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TotalcostComponent {
+export class TotalcostComponent implements OnChanges {
   @Input() totalCost: number;
   @Output() shownGross: EventEmitter<number> = new EventEmitter<number>();
   VAT: number = 1.23;
+  costThreshold: number = 10000;
+  isCostTooLow: boolean = false;
+
   showGross(): void {
-    setTimeout(() => {console.log('hey'); }, 1000);
+     setTimeout(() => {
+     console.log('hey');
+    }, 1000);
     this.shownGross.emit(this.totalCost * this.VAT);
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.isCostTooLow = changes['totalCost'].currentValue < this.costThreshold;
+  }
+
 }
